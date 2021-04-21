4d6faa1300ebfd5a361e10e0c7960c53e6a8b855
henkejson <54803889+henkejson@users.noreply.github.com>
2021-04-21 15:16:22 +0200

    Möjligt att bläddra mellan 8 frågor

--- hast_app/hast_app/lib/question.dart
+++ hast_app/hast_app/lib/question.dart
@@ -3,26 +3,122 @@ import 'package:flutter/material.dart';
 /// A function used in many places that takes an int and returns a Color.
 typedef _ColorCallBack = Color Function(int n, [int intensity]);
 
-class Question extends StatelessWidget {
+class QuestionContent {
+  String questionText = '';
+  String option1 = '';
+  String option2 = '';
+  String option3 = '';
+  String option4 = '';
 
-  int question_number = 1;
-  String question_title = 'Lifestyle and Work Assessment';
-  String question_text = 'Reactivity vs. finding \'the space\'';
-  String option1 = 'You often overreact to other people, since you are often '
-      'treated unfairly. Sometimes you feel the world is against you.';
-  String option2 = 'You see that you react in ways that don’t work in many '
-      'situations. You don’t feel good about it, but you have no idea how to '
-      'change your behaviour.';
-  String option3 = 'You often catch yourself when you overreact, you find ‘the '
-      'space’, and from there you can create a more balanced response.';
-  String option4 = 'As you let your reactions be instead of trying to change them,'
-      '  you often get insights and see new possibilities.';
+  QuestionContent(this.questionText, this.option1, this.option2, this.option3,
+      this.option4);
+}
 
-  String subOption1 = '1'; //Vad betyder egentligen graderingen som görs i fas två
-  String subOption2 = '2';
-  String subOption3 = '3';
+List questionList = [];
+int question_number = 0;
+
+String question_title = 'Lifestyle and Work Assessment';
+
+String question1_text = 'Reactivity vs. finding \'the space\'';
+
+String option1_1 = 'You often overreact to other people, since you are often '
+    'treated unfairly. Sometimes you feel the world is against you.';
+
+String option1_2 = 'You see that you react in ways that don’t work in many '
+    'situations. You don’t feel good about it, but you have no idea how to '
+    'change your behaviour.';
+
+String option1_3 = 'You often catch yourself when you overreact, you find ‘the '
+    'space’, and from there you can create a more balanced response.';
+
+String option1_4 =
+    'As you let your reactions be instead of trying to change them,'
+    '  you often get insights and see new possibilities.';
+
+String subOption1 = '1';
+String subOption2 = '2';
+
+String subOption3 = '3';
+
+String question2_title = 'Lifestyle and Work Assessment';
+
+String question2_text = 'Relaxed or Stressed';
+
+String option2_1 =
+    'You are very stressed. You notice your physical health is suffering. You are not sure how long you can keep this up without going into Burnout.';
+
+String option2_2 =
+    'You are stressed most days; you are frequently anxious or worried and you are not sure what to do to change things for the better. Sometimes, you might feel a little better, but stress comes back very quickly.';
+
+String option2_3 =
+    'You have reached a place where you feel relaxed a lot of the time, even if you feel stressed now and again. You are alert for when you have stressful thinking and can move relatively easily beyond stress into relaxation.';
+
+String option2_4 =
+    'You are relaxed, yet responsive to what happens in life and you seem to be able to handle big and small challenges with grace and ease. Others wonder how you can be so productive and yet so relaxed.';
+
+String nextButton = "Next";
 
 
+class Question extends StatefulWidget {
+  Question() {
+    question_number = 0;
+
+    _updateQuestionContent();
+  }
+
+  _updateQuestionContent() {
+    //Add question 1 text
+    questionList.insert(0, new QuestionContent(
+            question1_text, option1_1, option1_2, option1_3, option1_4));
+
+    //Add question 2 text
+    questionList.insert(
+        1,
+        new QuestionContent(
+            question2_text, option2_1, option2_2, option2_3, option2_4));
+
+    //Add question 3 text
+    questionList.insert(
+        2,
+        new QuestionContent(
+            question1_text, option1_1, option1_2, option1_3, option1_4));
+
+    //Add question 4 text
+    questionList.insert(
+        3,
+        new QuestionContent(
+            question2_text, option2_1, option2_2, option2_3, option2_4));
+
+    //Add question 5 text
+    questionList.insert(
+        4,
+        new QuestionContent(
+            question1_text, option1_1, option1_2, option1_3, option1_4));
+
+    //Add question 6 text
+    questionList.insert(
+        5,
+        new QuestionContent(
+            question2_text, option2_1, option2_2, option2_3, option2_4));
+
+    //Add question 7 text
+    questionList.insert(
+        6,
+        new QuestionContent(
+            question1_text, option1_1, option1_2, option1_3, option1_4));
+
+    //Add question 8 text
+    questionList.insert(
+        7,
+        new QuestionContent(
+            question2_text, option2_1, option2_2, option2_3, option2_4));
+  }
+
+  @override
+  _QuestionState createState() => _QuestionState();
+}
+
+class _QuestionState extends State<Question> {
   /// Takes an int and returns a Color (has an optional intensity argument).
   Color _getColor(int n, [int intensity = 200]) {
     switch (n) {
@@ -41,23 +137,85 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    if (question_number == 7){
+      nextButton = 'Result';
+    } else
+      nextButton = 'Next';
+
+    QuestionContent currentQuestion = questionList[question_number];
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text(question_title),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
-          child: Column(
-            children: <Widget>[
-              _QuestionText(question_text),
-              _CreateAnswers(option1, option2, option3, option4, _getColor),
-              //_CreateFollowUpAnswers(0)
-            ],
-          ),
+        appBar: AppBar(
+          title: Text(question_title),
         ),
-      )
-    );
+        body: Center(
+            child: Column(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 16),
+            child: Column(
+              children: <Widget>[
+                _QuestionText(currentQuestion.questionText),
+                _CreateAnswers(
+                    currentQuestion.option1,
+                    currentQuestion.option2,
+                    currentQuestion.option3,
+                    currentQuestion.option4,
+                    _getColor),
+                //_CreateFollowUpAnswers(0)
+                Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 64, 16, 16),
+                    child: Row(
+                      children: <Widget>[
+                        TextButton(
+                          style: ButtonStyle(
+                            foregroundColor: MaterialStateProperty.resolveWith(
+                                (Set<MaterialState> states) {
+                              return Colors.white;
+                            }),
+                            backgroundColor: MaterialStateProperty.resolveWith(
+                                (Set<MaterialState> states) {
+                              return Colors.red[800];
+                            }),
+                          ),
+                          onPressed: () {
+                            if (question_number >= 1) {
+                              question_number--; //TODO
+                              setState(() {});
+                            }
+                          },
+                          child: Text('Back'),
+                        ),
+                        Spacer(),
+                        Text((question_number+1).toString()),
+                        Spacer(),
+                        TextButton(
+                          style: ButtonStyle(
+                            foregroundColor: MaterialStateProperty.resolveWith(
+                                (Set<MaterialState> states) {
+                              return Colors.white;
+                            }),
+                            backgroundColor: MaterialStateProperty.resolveWith(
+                                (Set<MaterialState> states) {
+                              return Colors.red[800];
+                            }),
+                          ),
+                          onPressed: () {
+                            if (question_number <= 6) {
+                              question_number++; //TODO
+                              setState(() {});
+                            } else if (question_number == 7) {
+                              Navigator.pushNamed(context, '/result');
+                            }
+                          },
+                          child: Text(nextButton),
+                        ),
+                      ],
+                    ))
+              ],
+            ),
+          ),
+        ])));
   }
 }
 
@@ -65,7 +223,6 @@ class Question extends StatelessWidget {
 ///
 /// Has arguments 4 strings and a function.
 class _CreateAnswers extends StatelessWidget {
-
   final String a1;
   final String a2;
   final String a3;
@@ -79,16 +236,15 @@ class _CreateAnswers extends StatelessWidget {
   Widget build(BuildContext context) {
     return IntrinsicHeight(
         child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child: _AnswerText(a1, 0, colorFunction)),
-            Expanded(child: _AnswerText(a2, 1, colorFunction)),
-            Expanded(child: _AnswerText(a3, 2, colorFunction)),
-            Expanded(child: _AnswerText(a4, 3, colorFunction)),
-          ],
-        )
-    );
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(child: _AnswerText(a1, 0, colorFunction)),
+        Expanded(child: _AnswerText(a2, 1, colorFunction)),
+        Expanded(child: _AnswerText(a3, 2, colorFunction)),
+        Expanded(child: _AnswerText(a4, 3, colorFunction)),
+      ],
+    ));
   }
 }
 
@@ -105,15 +261,17 @@ class _CreateFollowUpAnswers extends StatelessWidget {
   Widget build(BuildContext context) {
     return IntrinsicHeight(
         child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child: _FollowUpAnswerText(a1*3+1, colorFunction(a1, 300))),
-            Expanded(child: _FollowUpAnswerText(a1*3+2, colorFunction(a1, 300))),
-            Expanded(child: _FollowUpAnswerText(a1*3+3, colorFunction(a1, 300))),
-          ],
-        )
-    );
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Expanded(
+            child: _FollowUpAnswerText(a1 * 3 + 1, colorFunction(a1, 300))),
+        Expanded(
+            child: _FollowUpAnswerText(a1 * 3 + 2, colorFunction(a1, 300))),
+        Expanded(
+            child: _FollowUpAnswerText(a1 * 3 + 3, colorFunction(a1, 300))),
+      ],
+    ));
   }
 }
 
@@ -158,21 +316,18 @@ class _AnswerText extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
       child: Container(
         color: colorFunction(number),
-        child: Column(
-          children: [
-            Padding(
+        child: Column(children: [
+          Padding(
               padding: EdgeInsets.fromLTRB(8, 8, 8, 8),
               child: Text(
                 atext,
                 style: TextStyle(
                   fontSize: 14,
                 ),
-              )
-            ),
-            Spacer(),
-            _CreateFollowUpAnswers(number, colorFunction)
-          ]
-        ),
+              )),
+          Spacer(),
+          _CreateFollowUpAnswers(number, colorFunction)
+        ]),
       ),
     );
   }
@@ -196,7 +351,9 @@ class _FollowUpAnswerText extends StatelessWidget {
           primary: color,
           onPrimary: Colors.black,
         ),
-        onPressed: () {print('$number points');},
+        onPressed: () {
+          print('$number points');
+        },
         child: Container(
             color: Colors.transparent,
             child: Padding(
@@ -206,9 +363,7 @@ class _FollowUpAnswerText extends StatelessWidget {
                   style: TextStyle(
                     fontSize: 14,
                   ),
-                )
-            )
-        ),
+                ))),
       ),
     );
   }
