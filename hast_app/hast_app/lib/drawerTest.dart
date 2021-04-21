ed8bdd737a742d477cef02fe179eecf999e66a6c
Erik Blomberg <31245545+RedSpettan@users.noreply.github.com>
2021-04-21 15:20:22 +0200

    Added a ListBuilder to the Navigation Drawer

--- hast_app/hast_app/lib/drawerTest.dart
+++ hast_app/hast_app/lib/drawerTest.dart
@@ -10,20 +10,23 @@ class DrawerTest extends StatefulWidget {
 }
 
 class _DrawerTestState extends State<DrawerTest> {
-  int _selectedDestination = 0;
+  int _selectedDestination = 1;
+  int _questionCount = 8;
+  
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildList(BuildContext context){
+
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
 
-    return Row(
-      children: [
-        Drawer(
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: <Widget>[
+    return ListView.builder(
+      itemCount: _questionCount,
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, i){
+
+        if(i == 0){
+          return Column(
+            children: [
               Padding(
                 padding: const EdgeInsets.all(16.0),
                 child: Text(
@@ -35,56 +38,31 @@ class _DrawerTestState extends State<DrawerTest> {
                 height: 1,
                 thickness: 1,
               ),
-              ListTile(
-                leading: Icon(Icons.label, color: Colors.green,),
-                title: Text('Question 1'),
-                selected: _selectedDestination == 0,
-                onTap: () => selectDestination(0),
-              ),
-              ListTile(
-                leading: Icon(Icons.label),
-                title: Text('Question 2'),
-                selected: _selectedDestination == 1,
-                onTap: () => selectDestination(1),
-              ),
-              ListTile(
-                leading: Icon(Icons.label),
-                title: Text('Question 3'),
-                selected: _selectedDestination == 2,
-                onTap: () => selectDestination(2),
-              ),
-              ListTile(
-                leading: Icon(Icons.label),
-                title: Text('Question 4'),
-                selected: _selectedDestination == 3,
-                onTap: () => selectDestination(3),
-              ),
-              ListTile(
-                leading: Icon(Icons.label),
-                title: Text('Question 5'),
-                selected: _selectedDestination == 4,
-                onTap: () => selectDestination(4),
-              ),
-              ListTile(
-                leading: Icon(Icons.label),
-                title: Text('Question 6'),
-                selected: _selectedDestination == 5,
-                onTap: () => selectDestination(5),
-              ),
-              ListTile(
-                leading: Icon(Icons.label),
-                title: Text('Question 7'),
-                selected: _selectedDestination == 6,
-                onTap: () => selectDestination(6),
-              ),
-              ListTile(
-                leading: Icon(Icons.label),
-                title: Text('Question 8'),
-                selected: _selectedDestination == 7,
-                onTap: () => selectDestination(7),
-              ),
             ],
-          ),
+          );
+        }
+        return ListTile(
+          leading: Icon(Icons.label),
+          title: Text('Item ' + i.toString()),
+          selected: _selectedDestination == i,
+          onTap: () => selectDestination(i),
+        );
+      }
+    );
+
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    return Row(
+      children: [
+        Drawer(
+          child: _buildList(context)
         ),
         VerticalDivider(
           width: 1,
