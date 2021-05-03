7144f6ffd1781f667ba15d38d70aed82ba3e4a0c
Louisetranborg <tranborg@student.chalmers.se>
2021-05-03 20:17:22 +0200

    Changed one title and all data for the overview is available

--- hast_app/hast_app/lib/screen/result_page.dart
+++ hast_app/hast_app/lib/screen/result_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hast_app/common/question_content.dart';
 import 'package:hast_app/models/result_model.dart';
 import 'package:provider/provider.dart';
 
@@ -10,27 +11,41 @@ class ResultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: sätt text till den som matchar mängden poäng!
     ResultModel result = context.watch<ResultModel>();
+    List<QuestionContent> questions = result.quizModel.questions;
+
+    for(var question in questions){ //här har vi allt vi behöver för översikten
+      print(question.question);
+      print((question.chosenSubAlternative + 1) + (question.chosenAlternative * 3));
+    }
+
     scoreText = result.text;
     score = result.score;
     final theme = Theme.of(context);
 
+
+
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Score: $score',
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+          Text('Your score: $score' + "/96",
+              //The final score after a evaluation.
               textAlign: TextAlign.center,
               style: theme.textTheme.headline4),
           SizedBox(height: 50),
           Container(
               width: MediaQuery.of(context).size.width - 100,
-              child: Text('$scoreText',
+              child: Text('$scoreText', //the corresponding text for the score
                   textAlign: TextAlign.center,
                   style: theme.textTheme.headline5)),
           SizedBox(height: 100),
+
+
+
+
+
           TextButton(
             style: ButtonStyle(
               foregroundColor: MaterialStateProperty.resolveWith(
