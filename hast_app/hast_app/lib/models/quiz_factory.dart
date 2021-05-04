82bd322e14b63fdb101702d74437a24e2320da53
Erik Blomberg <31245545+RedSpettan@users.noreply.github.com>
2021-05-04 12:03:35 +0200

    We can now read JSON files from Quiz Factory
    Co-Authored-By: callec <31653738+callec@users.noreply.github.com>

--- hast_app/hast_app/lib/models/quiz_factory.dart
+++ hast_app/hast_app/lib/models/quiz_factory.dart
@@ -1,8 +1,27 @@
 import 'package:hast_app/common/question_content.dart';
 import 'package:hast_app/common/quiz_content.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'dart:async';
+
 
 class QuestionFactory {
+
+  static Future<String> getJson(){
+    return rootBundle.loadString('ReflectionQuiz.json');
+  }
+
+  static void getValue() async{
+    print(await getJson());
+  }
+
+
+
+
   static QuizContent createStandardQuiz() {
+
+
+    getValue();
+
     List<QuestionContent> questionList = [];
 
     String question_title = 'Lifestyle and Work Assessment';
