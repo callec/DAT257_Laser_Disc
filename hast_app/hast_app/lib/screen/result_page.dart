2c6be5bef5566a15a3dd46da78c2b803f5f928d9
Louisetranborg <tranborg@student.chalmers.se>
2021-05-06 19:04:10 +0200

    fixed indentation

--- hast_app/hast_app/lib/screen/result_page.dart
+++ hast_app/hast_app/lib/screen/result_page.dart
@@ -18,22 +18,18 @@ class ResultPage extends StatelessWidget {
     final theme = Theme.of(context);
 
     return Scaffold(
-
         appBar: AppBar(
           //title: Text(context.read<QuizModel>().title),
           title: HastLogga(),
           automaticallyImplyLeading: false,
           backgroundColor: theme.backgroundColor,
         ),
-
-        body: Center(
-
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  SizedBox(height: 30),
-          Text('Your score is: $score' + "/96",
-              //The final score after a evaluation.
+        body: Center(child:
+        Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          SizedBox(height: 30),
+          Text('Your score is: $score' + "/96", //The final score after a evaluation.
               textAlign: TextAlign.center,
               style: theme.textTheme.headline4),
           SizedBox(height: 30),
@@ -44,96 +40,67 @@ class ResultPage extends StatelessWidget {
                   style: theme.textTheme.headline5)),
           SizedBox(height: 20),
 
+          new Expanded(child: //the overview
+          Theme(
+            data: Theme.of(context).copyWith(dividerColor: theme.accentColor),
+            child:
+            Padding( //to get the overview in the middle and proper adjust when the window size is changed.
+              padding: EdgeInsets.only(
+                  left: MediaQuery.of(context).size.width * 0.12,
+                  right: MediaQuery.of(context).size.width * 0.12),
+              child:
+              ListView( //wrapped in a listView to be scrollable.
+                  children: <Widget>[
+                IgnorePointer(child: //wrapped in a IgnorePointer to remove clickable event.
+                SingleChildScrollView(
+                    scrollDirection: Axis.vertical,
+                    child:
+                    DataTable( columns: const <DataColumn>[
+                      DataColumn(
+                        label: Text('Question', style: TextStyle(fontStyle: FontStyle.italic))),
+                      DataColumn(
+                        label: Text('Text', style: TextStyle(fontStyle: FontStyle.italic))),
+                      DataColumn(
+                        label: Text('Score', style: TextStyle(fontStyle: FontStyle.italic))),
+                          ],
+                      rows:
+                      questions.map(((element) => DataRow( // Loops through dataColumnText, each iteration assigning the value to element
+                        //fills the dataTable with data from the list.
+                        cells: <DataCell>[
+                          DataCell(Text("      " + element.number.toString())),
+                          DataCell(Text(element.question)),
+                          DataCell(Text(((element.chosenSubAlternative + 1) + element.chosenAlternative * 3).toString() + "/12"))
+                        ],
+                      ))).toList())))
+              ]
+              )))),
 
-          new Expanded( //the overview
-              child: Theme(
-                  data: Theme.of(context).copyWith(
-                      dividerColor: theme.accentColor
-                  ),
-                      child: Padding( //to get the overview in the middle and proper adjust when the window size is changed.
-                        padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.12, right: MediaQuery.of(context).size.width * 0.12),
-                        child: ListView( //wrapped in a listView to be scrollable.
-                            children: <Widget>[
-                              IgnorePointer(child: SingleChildScrollView( //wrapped in a IgnorePointer to remove clickable event.
-                                  scrollDirection: Axis.vertical,
+          SizedBox(
+            height: 20),
 
-                                  child: DataTable(
-                                    columns: const <DataColumn>[
-                                      DataColumn(
+         SizedBox(//button wrapped in sizeBox to be able to change its size.
+           width: 100, height: 40,
+             child: TextButton(
+               style: ButtonStyle(
+                 foregroundColor: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+                    return theme.backgroundColor;
+                  }),
+                 backgroundColor: MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+                    return theme.accentColor;
+                  }),
+                ),
+               onPressed: () {
+                 Navigator.pushNamed(context, '/');
+                },
+               child: Text('Home'),
+              )),
 
-                                        label: Text(
-                                          'Question',
-                                          style: TextStyle(fontStyle: FontStyle.italic),
-
-                                        ),
-                                      ),
-                                      DataColumn(
-                                        label: Text(
-                                          'Text',
-                                          style: TextStyle(fontStyle: FontStyle.italic),
-                                        ),
-                                      ),
-                                      DataColumn(
-                                        label: Text(
-                                          'Score',
-                                          style: TextStyle(fontStyle: FontStyle.italic),
-                                        ),
-                                      ),
-                                    ],
-                                    rows:
-                                    questions // Loops through dataColumnText, each iteration assigning the value to element
-                                        .map(
-
-                                      ((element) => DataRow( //fills the dataTable with data from the list.
-
-                                        cells: <DataCell>[
-
-                                          DataCell(Text("      "+element.number.toString())),
-                                          DataCell(Text(element.question)),
-                                          DataCell(Text(((element.chosenSubAlternative+1)+element.chosenAlternative*3).toString()+"/12")),
-                                          //Extracting from Map element the value
-
-                                        ],
-                                      )),
-                                    )
-                                        .toList(),
-                                  )))
-
-                            ]
-                        ),
-                      )
-                        ,
-                    )),
-
-          SizedBox(height: 20,),
-
-          SizedBox( //button wrapped in sizeBox to be able to change its size.
-            width: 100,
-            height: 40,
-            child: TextButton(
-              style: ButtonStyle(
-                foregroundColor: MaterialStateProperty.resolveWith(
-                        (Set<MaterialState> states) {
-                      return theme.backgroundColor;
-                    }),
-                backgroundColor: MaterialStateProperty.resolveWith(
-                        (Set<MaterialState> states) {
-                      return theme.accentColor;
-                    }),
-              ),
-              onPressed: () {
-                Navigator.pushNamed(context, '/');
-              },
-              child: Text('Home'),
-
-            )
-          ),
-                  SizedBox(height: 20),
+              SizedBox(height: 20),
         ])));
-
   }
 }
-class HastLogga extends StatelessWidget{
+
+class HastLogga extends StatelessWidget {
   Widget build(BuildContext context) {
     return Image.asset(
       "assets/images/hastlogga.png",
