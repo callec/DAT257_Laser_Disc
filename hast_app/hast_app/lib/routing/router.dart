b67ce9b8280c3292925cebdab2c0e4ebf740ef2f
henkejson <54803889+henkejson@users.noreply.github.com>
2021-05-11 12:44:33 +0200

    Revert "we can get the query of the home page"
    This reverts commit 126fcdb8e7ba53621e07d5d4a5c5d25976d6b6d9.

--- hast_app/hast_app/lib/routing/router.dart
+++ hast_app/hast_app/lib/routing/router.dart
@@ -4,23 +4,11 @@ import 'package:hast_app/screen/home_page.dart';
 import 'package:hast_app/screen/quiz_page.dart';
 import 'package:hast_app/screen/result_page.dart';
 import 'package:hast_app/screen/undefined_page.dart';
-import 'package:hast_app/routing/string_extensions.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
-
-  var routingData = settings.name!.getRoutingData;
-
-  switch (routingData.route) {
+  switch (settings.name) {
     case HomeRoute:
-
-      var q = routingData['q'];
-
-      if (q != null){
-        return MaterialPageRoute(builder: (context) => HomePage(q));
-      } else {
-        return MaterialPageRoute(builder: (context) => UndefinedPage());
-      }
-
+      return MaterialPageRoute(builder: (context) => HomePage());
     case QuizRoute:
       return MaterialPageRoute(builder: (context) => QuizPage());
     case ResultRoute:
