69c574ff8d70981533e151d321307d82a2a3d3a6
callec <31653738+callec@users.noreply.github.com>
2021-05-14 17:30:20 +0200

    test container width

--- hast_app/hast_app/lib/screen/home_page.dart
+++ hast_app/hast_app/lib/screen/home_page.dart
@@ -119,9 +119,9 @@ class _HomePageState extends State<HomePage> {
   /// Displays information text on the home_page in a nice formatted way
   Widget _presentText(context, String presentingText, TextAlign alignment) {
     return Container(
-        // for small devices: keep text to 85% of the width, otherwise 600px to
+        // for small devices: keep text to 80% of the width, otherwise 600px to
         // keep ~70 character at a time in a paragraph for readability.
-        width: ResponsivePage.isMediumScreen(context) ? MediaQuery.of(context).size.width*0.85 : 600,
+        width: ResponsivePage.isMediumScreen(context) ? (MediaQuery.of(context).size.width*0.8) : 600,
         child: SingleChildScrollView(child: Column(children: [Text(presentingText,
             textAlign: alignment,
             style: Theme.of(context).textTheme.headline6)]),));
