41ae5b88d01d8db2112efece1be4203abe8817b3
callec <31653738+callec@users.noreply.github.com>
2021-05-19 14:51:43 +0200

    merge fixes

--- hast_app/hast_app/lib/screen/home_page.dart
+++ hast_app/hast_app/lib/screen/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hast_app/colors.dart';
 import 'package:hast_app/common/quiz_content.dart';
 import 'package:hast_app/models/quiz_factory.dart';
@@ -33,31 +34,7 @@ class _HomePageState extends State<HomePage> {
   // TODO formatting
   final String _infoHast = INFO_TEXT;
   final String _hastWelcome = HAST_WELCOME;
-  final String _infoHast = "*HAST Utveckling offer services of consulting, training, "
-      "seminars and coaching as experts in the areas of Leadership, Communication, "
-      "Effectiveness and Team Development.* The company was founded in 2001 and have since then "
-      "created results for managers, leaders and their personnel in over 40% of Sweden’s 100 largest companies. "
-      "They have travelled to do work in more than 15 countries and have had participants in our programs "
-      "from more than 40 different nations worldwide. \n\n*We at HAST Utveckling, provide the climate needed for "
-      "individuals and groups to take a good look at and discover key principles, behaviors and unaware preconceptions* "
-      "that keep us as human beings from reaching our true potential. These discoveries lead to waking people up to what "
-      "is really going on. From real insight you hardly need to tell people what to do. Instead, people have a natural tendency "
-      "to automatically implement behavior changes creating a new level of sustainable results. Fascinating! \n\n*These changes result in "
-      "increased peace of mind, focus and productivity* as well as better worklife balance and new possibilities of innovation."
-      " We turn stress to calm, conflict to teamwork, limitations to new results and managers to true leaders. \n\n*We asked some of "
-      "our most cherished customers,* such as The West Sweden Chamber of Commerce, Volvo Penta, ABB, RISE (Research Institutes of Sweden),"
-      " Preem, The Swedish Transport Administration and others, to describe, in their own words, what comes to mind when they think of HAST."
-      " Here are a few examples of their comments: \n\n";
-  final String _customerQuotes = "/“Great structures. Keep their promises. Clear processes and competent people.”/"
-      "\n\n/“Your level of competence is high, like many others, but you serve them with warmth and humor in a way that makes you stand out"
-      "from the crowd.”/ \n\n/“HAST dares to “touch on” matters that are not always comfortable to take a look at and that other suppliers just let go of.”/";
-final String _hastWelcome = "*Welcome!*\n\n"
-    "We at HAST Utveckling are pleased to offer our much-appreciated *Lifestyle & Work Assessment Tool.* When completed, it will point you towards the actual state of your work-life balance at this moment in time.\n\n"
-    "Take this opportunity, to pause in the midst of everyday worklife and perform this self-assessment which *focuses on 8 carefully selected perspectives of effectiveness.* Mastering these perspectives will collectively increase your production capacity to new unchartered levels of results.\n\n"
-    "*This tool will provide you with clarification* on what areas you have already successfully mastered and in what areas you could greatly benefit from new insights and tools.\n\n"
-    "We provide this tool for anyone who is curious as to where you are at today, compared to what is possible! *We at HAST, love honest conversations and transparency*, so… as you view your personal total score at the end of this self-assessment, you may decide to connect with us and have a coaching conversation with us to explore your missing pieces – or not. The choice is yours!\n\n"
-    "At your service,\n"
-    "The HAST Team";
+  final String _customerQuotes = HAST_QUOTES;
 
 
   _HomePageState(String query){
@@ -183,7 +160,16 @@ final String _hastWelcome = "*Welcome!*\n\n"
             quizModel.loadQuiz(quiz);
             Navigator.pushNamed(context, QuizRoute);
           },
-          child: Padding( padding: EdgeInsets.fromLTRB(8, 24 ,8,24), child: SimpleRichText("*Start the ${quiz.quizTitle} now!*", textAlign: TextAlign.center, textScaleFactor: 1.25, style: TextStyle(color: hastWhite)))
+          child: Padding( padding: EdgeInsets.fromLTRB(8, 24 ,8,24),
+              child: SimpleRichText(
+                  "*Start the ${quiz.quizTitle} now!*",
+                  textAlign: TextAlign.center,
+                  //textScaleFactor: 1.3,
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: hastWhite,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: GoogleFonts.roboto().fontFamily)))
       );
     } else {
       return ElevatedButton(
--- hast_app/hast_app/lib/screen/home_page_data.dart
+++ hast_app/hast_app/lib/screen/home_page_data.dart
@@ -1,20 +1,18 @@
 const String INFO_TEXT = "*HAST Utveckling offer services of consulting, training, "
-    "seminars and coaching as experts in the areas of Leadership, Communication, "
-    "Effectiveness and Team Development.* The company was founded in 2001 and have since then "
-    "created results for managers, leaders and their personnel in over 40% of Sweden’s 100 largest companies. "
-    "They have travelled to do work in more than 15 countries and have had participants in our programs "
-    "from more than 40 different nations worldwide. \n\n*We at HAST Utveckling, provide the climate needed for "
-    "individuals and groups to take a good look at and discover key principles, behaviors and unaware preconceptions* "
-    "that keep us as human beings from reaching our true potential. These discoveries lead to waking people up to what "
-    "is really going on. From real insight you hardly need to tell people what to do. Instead, people have a natural tendency "
-    "to automatically implement behavior changes creating a new level of sustainable results. Fascinating! \n\n*These changes result in "
-    "increased peace of mind, focus and productivity* as well as better worklife balance and new possibilities of innovation."
-    " We turn stress to calm, conflict to teamwork, limitations to new results and managers to true leaders. \n\n*We asked some of "
-    "our most cherished customers,* such as The West Sweden Chamber of Commerce, Volvo Penta, ABB, RISE (Research Institutes of Sweden),"
-    " Preem, The Swedish Transport Administration and others, to describe, in their own words, what comes to mind when they think of HAST."
-    " Here are a few examples of their comments: \n\n    /“Great structures. Keep their promises. Clear processes and \n    competent people.”/"
-    "\n\n    /“Your level of competence is high, like many others, but you\n    serve them with warmth and humor in a way that makes\n    you stand out "
-    "from the crowd.”/ \n\n    /“HAST dares to “touch on” matters that are not always\n    comfortable to take a look at and that other suppliers just\n    let go of.”/";
+  "seminars and coaching as experts in the areas of Leadership, Communication, "
+  "Effectiveness and Team Development.* The company was founded in 2001 and have since then "
+  "created results for managers, leaders and their personnel in over 40% of Sweden’s 100 largest companies. "
+  "They have travelled to do work in more than 15 countries and have had participants in our programs "
+  "from more than 40 different nations worldwide. \n\n*We at HAST Utveckling, provide the climate needed for "
+  "individuals and groups to take a good look at and discover key principles, behaviors and unaware preconceptions* "
+  "that keep us as human beings from reaching our true potential. These discoveries lead to waking people up to what "
+  "is really going on. From real insight you hardly need to tell people what to do. Instead, people have a natural tendency "
+  "to automatically implement behavior changes creating a new level of sustainable results. Fascinating! \n\n*These changes result in "
+  "increased peace of mind, focus and productivity* as well as better worklife balance and new possibilities of innovation."
+  " We turn stress to calm, conflict to teamwork, limitations to new results and managers to true leaders. \n\n*We asked some of "
+  "our most cherished customers,* such as The West Sweden Chamber of Commerce, Volvo Penta, ABB, RISE (Research Institutes of Sweden),"
+  " Preem, The Swedish Transport Administration and others, to describe, in their own words, what comes to mind when they think of HAST."
+  " Here are a few examples of their comments: \n\n";
 
 const String HAST_WELCOME = "*Welcome!*\n\n"
     "We at HAST Utveckling are pleased to offer our much-appreciated *Lifestyle & Work Assessment Tool.* When completed, it will point you towards the actual state of your work-life balance at this moment in time.\n\n"
@@ -23,3 +21,8 @@ const String HAST_WELCOME = "*Welcome!*\n\n"
     "We provide this tool for anyone who is curious as to where you are at today, compared to what is possible! *We at HAST, love honest conversations and transparency*, so… as you view your personal total score at the end of this self-assessment, you may decide to connect with us and have a coaching conversation with us to explore your missing pieces – or not. The choice is yours!\n\n"
     "At your service,\n"
     "The HAST Team";
+
+const String HAST_QUOTES = "/“Great structures. Keep their promises. Clear processes and competent people.”/"
+    "\n\n/“Your level of competence is high, like many others, but you serve them with warmth and humor in a way that makes you stand out"
+    "from the crowd.”/ \n\n/“HAST dares to “touch on” matters that are not always comfortable to take a look at and that other suppliers just let go of.”/";
+
